f51aaf64a7be15a7cc7246289932a6d3a59dae70
giacomo.rapposelli <giacomo.rapposelli@booming-games.com>
2021-09-20 14:50:28 +0200

    js and react test solutions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // We import the object from the data file. Inside that object there is a function to get players data
-const data = require("./data");
-
+const data = require('./data');
+const players = data.getPlayers();
 /**
  * Test 1
  * Write a function to log in the console the players data with this format:
@@ -11,9 +11,22 @@ const data = require("./data");
  * PLAYER 2...
  */
 
- // Your code
+// Your code
+const logPlayersData = () => {
+    const capFirstChar = (str) =>
+        `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+    for (const [index, player] of players.entries()) {
+        console.log(
+            `PLAYER ${index + 1}\nNAME: ${capFirstChar(
+                player.name
+            )}\nLASTNAME: ${capFirstChar(
+                player.lastname
+            )}\nPOSITION: ${capFirstChar(player.position)}`
+        );
+    }
+};
 
-
+logPlayersData();
 
 /**
  * Test 2
@@ -21,39 +34,72 @@ const data = require("./data");
  */
 
 // Your code
+const logPlayersNamesList = () => {
+    const playerNames = players.map((player) => player.name);
+    const descendingOrder = playerNames.sort((a, b) => b.length - a.length);
+    console.log(descendingOrder);
+};
 
-
-
-
+logPlayersNamesList();
 
 /**
  * Test 3
  * Write a function to log in the console the average number of goals there will be in a match if all the players in the data play on it
  * scoringChance means how many goals per 100 matches the player will score
- * Example: 10 players play in a match, all of them has 0.11 scoringChance, the result will be 1.1 average goals 
+ * Example: 10 players play in a match, all of them has 0.11 scoringChance, the result will be 1.1 average goals
  * Output example -> Goals per match: 2.19
  */
 
 // Your code
+const logAverageGoals = () => {
+    const scoringChances = players.map((player) => player.scoringChance);
+    const averageGoals = scoringChances.reduce((a, b) => a + +b) / 100;
+    console.log(`Goals per match: ${averageGoals}`);
+};
 
-
+logAverageGoals();
 
 /**
  * Test 4
  * Write a function that accepts a name, and logs the position of the player with that name (by position it means striker, goalkeeper...)
  */
-
 // Your code
+const logPlayerPosition = (name) => {
+    const playerFound = players.find(
+        (player) => player.name.toLowerCase() === name.toLowerCase()
+    );
+    playerFound
+        ? console.log(playerFound.position)
+        : console.log('player not found');
+};
 
-
+logPlayerPosition('Timo');
 
 /**
  * Test 5
  * Write a function that splits all the players randomly into 2 teams, Team A and Team B. Both teams should have same number of players.
  * The function should log the match score, using the average number of goals like the Test 3 and rounding to the closest integer
  * Example:
- *      Team A has 4 players, 2 with 50 scoringChance and 2 with 70 scoringChance. 
+ *      Team A has 4 players, 2 with 50 scoringChance and 2 with 70 scoringChance.
  *      The average score for the team would be 2.4 (50+50+70+70 / 100), and the closest integer is 2, so the Team A score is 2
  */
 
 // Your code
+const logMatchScore = () => {
+    const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+    const randomOrderPlayers = shuffleArray(players);
+    const teamA = randomOrderPlayers.slice(0, randomOrderPlayers.length / 2);
+    const teamB = randomOrderPlayers.slice(randomOrderPlayers.length / 2);
+    const teams = [teamA, teamB];
+    const result = [];
+    for (const team of teams) {
+        const scoringChances = team.map((player) => player.scoringChance);
+        const teamScore = Math.round(
+            scoringChances.reduce((a, b) => +a + +b) / 100
+        );
+        result.push(teamScore);
+    }
+    console.log(result.join('-'));
+};
+
+logMatchScore();
--- reactTest.tsx
+++ reactTest.tsx
@@ -1,17 +1,17 @@
 /**
  * If you want to prove your React skills, please try this test.
- * 
- * TIPS: 
+ *
+ * TIPS:
  * - These are a theoretical questions, there is no need to install packages or run the code.
  */
 
-import React from "react";
+import React from 'react';
 
 /**
  * TEST 1
- * 
+ *
  * This test is a simple one with conditional rendering.
- * 
+ *
  * This component has to return:
  *  The name in blue if the age is greater than or equal than 18.
  *  The name in red otherwise.
@@ -19,28 +19,29 @@ import React from "react";
 
 interface ITest1ComponentProps {
     name: string;
-    age:number;
+    age: number;
 }
 
-export const Test1Component = (props: ITest1ComponentProps) => {
-
-}
-
+export const Test1Component = (props: ITest1ComponentProps) =>
+    props.age >= 18 ? (
+        <h2 style={{ color: 'blue' }}>{props.name}</h2>
+    ) : (
+        <h2 style={{ color: 'red' }}>{props.name}</h2>
+    );
 
 /**
  * TEST 2
- * 
+ *
  * This is test is about handling changes of the data from an API.
- * 
+ *
  * Like test 1, this component has to return:
  *  The name in blue if the age is greater than or equal than 18.
  *  The name in red otherwise.
- * 
+ *
  * The difference is we don't have the age, we need to use the function below to get it.
  * The name from the parent can change any time, we have ensure the component rerenders if it happens
  * Getting data from an API (we simulate it with a timeout) is async, please be sure the code updates when we get the response back from the API
  */
-
 
 /**
  * This function accepts a name and simulates and API call to get the age of the person
@@ -48,25 +49,31 @@ export const Test1Component = (props: ITest1ComponentProps) => {
  * @param name Name of the person we want to find the age
  * @returns random integer from 0 to 39
  */
-const getAge = async (name: string): Promise<number>  => {
+const getAge = async (name: string): Promise<number> => {
     // This function calls an API and returns
     return new Promise((resolve, reject) => {
         return setTimeout(() => {
             resolve(Math.floor(Math.random() * 40));
-        },500);
-    })
-}
+        }, 500);
+    });
+};
 
 interface ITest2ComponentProps {
     name: string;
 }
 
 export const Test2Component = (props: ITest2ComponentProps) => {
+    const [age, setAge] = useState();
 
     useEffect(() => {
-    }, []);
-    
-}
+        getAge(props.name)
+            .then((res) => setAge(res))
+            .catch((err) => console.log(err));
+    }, [props.name]);
 
-
-
+    return age >= 18 ? (
+        <h2 style={{ color: 'blue' }}>{props.name}</h2>
+    ) : (
+        <h2 style={{ color: 'red' }}>{props.name}</h2>
+    );
+};
